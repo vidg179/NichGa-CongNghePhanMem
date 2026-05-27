c0d3acf00a6f7d9693298604d4873ad54bbad5fa
Nguyen Minh Nhat <nhatnguyenminh380@gmail.com>
2026-05-28 01:32:24 +0700

    CF-17: Nang cap trang quan ly san pham admin voi CRUD hoan chinh

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { CartProvider } from "./context/CartContext";
+import { Toaster } from "sonner";
 
 export default function App() {
   return (
     <CartProvider>
       <RouterProvider router={router} />
+      <Toaster position="top-right" richColors />
     </CartProvider>
   );
 }
